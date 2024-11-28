3fc79e9c0190488ffa3c67af60e80c4f7fd78784
Taylor Marlow <metallicax4xyou@gmail.com>
2024-11-27 23:55:16 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,37 @@ export default function HomePage() {
     <div>
       <section className="hero">
         <div className="container">
-          <h1>Start mining and get your (simulated) profit today!</h1>
-          <p>This is a research project simulating a cloud mining platform.</p>
-          <button>Start Mining (Simulation)</button>
+          <div className="stat_hero"> {/* Start of stat_hero */}
+            <div className="block_stat">
+              <img src="/invested.svg" alt="Invested" /> {/* Local image path */}
+              <h4>7.6m</h4>
+              <span>Invested</span>
+            </div>
+            <div className="block_stat">
+              <img src="/paid.svg" alt="Paid Out" /> {/* Local image path */}
+              <h4>2.1m</h4>
+              <span>Paid out</span>
+            </div>
+            <div className="block_stat">
+              <img src="/users.svg" alt="Users" /> {/* Local image path */}
+              <h4>1.2m</h4>
+              <span>Users</span>
+            </div>
+          </div> {/* End of stat_hero */}
+
+          <p>
+            Mining is one of the leading cryptocurrency mining platforms. No need to purchase hardware devices, manage devices, offering cryptocurrency mining capacities in every range - for newcomers. (This is a simulation)
+          </p>
+          <button>Start mining (Simulation)</button>
+
+          {/* Hero image (add later) */}
+          {/* <div className="image_hero">
+            <img src="/hero_img.png" alt="Hero Image" className="img-fluid" />
+          </div> */}
+
         </div>
       </section>
       {/* Other content can go here */}
     </div>
   );
-}
+      }
